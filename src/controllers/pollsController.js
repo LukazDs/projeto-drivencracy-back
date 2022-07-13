5691bc8fb98b5324f62d851c5bed2160186e9d7a
Lucas Duarte <lukazds2016@gmail.com>
2022-07-13 13:24:20 -0300

    Retornando status 200 em caso de sucesso ao criar uma votação

--- src/controllers/pollsController.js
+++ src/controllers/pollsController.js
@@ -4,11 +4,19 @@ import { pollSchema } from "../schemas/pollsSchema.js";
 export async function insertPoll(req, res) {
     const poll = req.body;
 
-    const validation = pollSchema.validate(poll)
+    const validation = pollSchema.validate(poll);
 
     if (validation.error) {
         return res.sendStatus(422);
     }
 
-    await db.collection("polls").insertOne(poll)
+    try {
+
+        await db.collection("polls").insertOne(poll)
+
+        res.status(201).send('Post criado com sucesso');
+
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
