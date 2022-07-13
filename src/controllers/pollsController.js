ef898f46726751ef2d62a27524fd05e5048c6f2d
Lucas Duarte <lukazds2016@gmail.com>
2022-07-13 13:19:53 -0300

    Analisando dados da requisição com joi para inserir na coleção polls

--- src/controllers/pollsController.js
+++ src/controllers/pollsController.js
@@ -1,8 +1,14 @@
 import { db } from "../dbStrategy/mongo.js";
+import { pollSchema } from "../schemas/pollsSchema.js";
 
 export async function insertPoll(req, res) {
     const poll = req.body;
 
+    const validation = pollSchema.validate(poll)
+
+    if (validation.error) {
+        return res.sendStatus(422);
+    }
+
     await db.collection("polls").insertOne(poll)
-    
 }
